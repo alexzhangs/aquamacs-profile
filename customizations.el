76b94660e60ff774889b7383f53bc52813f11ead
alex <alexzhangs@gmail.com>
2016-10-15 04:29:38 +0800

    Fix the jumpy scroll with Apple Magic Mouse

--- customizations.el
+++ customizations.el
@@ -176,3 +176,10 @@
               (t (goto-char skip-to)))))))
 
 (put 'downcase-region 'disabled nil)
+
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+
